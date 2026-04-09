fe419ed19ac63f95856d7a7830b9f233026fd6c4
tianyi <2487272145@qq.com>
2026-04-08 22:59:37 -0400

    update for N=1

--- Map_Tool/map_hypercube.py
+++ Map_Tool/map_hypercube.py
@@ -543,8 +543,8 @@ class MapHypercubeGame:
         self.color_map_f = np.array([], dtype=int)
 
     def can_analyze(self):
-        return True
         # return (len(self.med_group) + len(self.joint_group)) >= 2 and (len(self.fire_group) + len(self.joint_group)) >= 2
+        return (len(self.med_group) + len(self.joint_group)) >= 1 and (len(self.fire_group) + len(self.joint_group)) >= 1
 
     def group_for_kind(self, kind):
         if kind == MED:
